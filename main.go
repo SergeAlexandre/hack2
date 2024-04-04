af74f9e548fc53746f407bde5baa8210193c146f
SergeAlexandre <sa@broadsoftware.com>
2024-04-04 20:16:54 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Allo")
+
+	fmt.Println("Allo2")
+
+	fmt.Println("Allo3")
+
+	fmt.Println("Allo4")
+
+	fmt.Println("Allo5")
+
+	fmt.Println("Allo6")
+
+	fmt.Println("Allo7")
+}
